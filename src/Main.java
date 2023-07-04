c52bd977969041e1f22c542c4962257887de8562
АХОН <221042@astanait.edu.kz>
2023-07-04 23:46:05 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+         public class FibonacciSequence {
+        public static int fibonacci(int n) {
+            if (n < 0) {
+            }
+
+            if (n == 0) {
+                return 0;
+            } else if (n == 1) {
+                return 1;
+            }
+
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
